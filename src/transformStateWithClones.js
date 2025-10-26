db764e460f79ceb40d3e7c8f97507382b5a8a1c0
mist1que <allouser020220@gmail.com>
2025-10-26 19:53:15 +0200

    Cloning and transforming state

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,30 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const history = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    }
+
+    if (action.type === 'removeProperties') {
+      currentState = Object.fromEntries(
+        Object.entries(currentState).filter(
+          ([key]) => !action.keysToRemove.includes(key),
+        ),
+      );
+    }
+
+    history.push({ ...currentState });
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
